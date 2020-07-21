bd5acec6396f6f751c0e12772b7e86ea68f32a3e
Christian Muehlhaeuser <muesli@gmail.com>
2020-07-21 05:06:45 +0200

    Display 'today' if the recent release happened within the last 24 hours

--- humanize.go
+++ humanize.go
@@ -10,6 +10,10 @@ import (
 func humanized(t interface{}) string {
 	switch v := t.(type) {
 	case time.Time:
+		if time.Since(v) <= time.Hour*24 {
+			return "today"
+		}
+
 		return humanize.Time(v)
 	default:
 		return fmt.Sprintf("%v", t)
