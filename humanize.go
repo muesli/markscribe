ed861b8d83c934555c7013d6d6a9c8a5e08ce4bd
Christian Muehlhaeuser <muesli@gmail.com>
2020-07-22 06:49:27 +0200

    Flatten timestamps to prevent updating README too often

--- humanize.go
+++ humanize.go
@@ -10,6 +10,9 @@ import (
 func humanized(t interface{}) string {
 	switch v := t.(type) {
 	case time.Time:
+		// flatten time to prevent updating README too often:
+		v = time.Date(v.Year(), v.Month(), v.Day(), 0, 0, 0, 0, v.Location())
+
 		if time.Since(v) <= time.Hour*24 {
 			return "today"
 		}
