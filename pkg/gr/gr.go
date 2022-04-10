feab69a8b1a656f2348c4e86244d924c86bcda4f
Techassi <git@techassi.dev>
2022-04-10 14:50:27 +0200

    Re-add changes

--- pkg/gr/gr.go
+++ pkg/gr/gr.go
@@ -1,19 +1,42 @@
-package main
+package gr
 
 import (
+	"os"
+
+	"github.com/KyleBanks/goodreads"
 	"github.com/KyleBanks/goodreads/responses"
 )
 
-func goodReadsReviews(count int) []responses.Review {
-	reviews, err := goodReadsClient.ReviewList(goodReadsID, "read", "date_read", "", "d", 1, count)
+type Client struct {
+	client *goodreads.Client
+	token  string
+	id     string
+}
+
+// New creates and returns a new GoodReads client.
+func New() *Client {
+	token := os.Getenv("GOODREADS_TOKEN")
+	id := os.Getenv("GOODREADS_USER_ID")
+
+	return &Client{
+		client: goodreads.NewClient(token),
+		token:  token,
+		id:     id,
+	}
+}
+
+// GetReviews retrieves 'n' reviews.
+func (c *Client) GetReviews(n int) []responses.Review {
+	reviews, err := c.client.ReviewList(c.id, "read", "date_read", "", "d", 1, n)
 	if err != nil {
 		panic(err)
 	}
 	return reviews
 }
 
-func goodReadsCurrentlyReading(count int) []responses.Review {
-	reviews, err := goodReadsClient.ReviewList(goodReadsID, "currently-reading", "date_updated", "", "d", 1, count)
+// GetCurrentlyReading retrieves 'n' currently read books.
+func (c *Client) GetCurrentlyReading(n int) []responses.Review {
+	reviews, err := c.client.ReviewList(c.id, "currently-reading", "date_updated", "", "d", 1, n)
 	if err != nil {
 		panic(err)
 	}
